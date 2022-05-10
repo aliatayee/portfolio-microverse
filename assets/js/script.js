ebc1076a8cd5ad201c2e0af6cfe9607976443941
aliatayee <aliaqaatayee@gmail.com>
2022-05-10 16:02:51 +0430

    Add: Mobile menu with hamburger navigation

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function menuFunction() {
+    const links = document.getElementById('mobile-menu-container');
+    if (links.style.display === 'block') {
+      links.style.display = 'none';
+    } else if (window.screen.width < 995) {
+      links.style.display = 'block';
+    } else {
+      links.style.display = 'none';
+    }
+  }
