2c7cbc48105e3c9b5a6864b1297bda38fd02f783
aliatayee <aliaqaatayee@gmail.com>
2022-05-12 18:07:27 +0430

    Update: popup modal to pass data deynamicly

--- index.js
+++ index.js
@@ -24,54 +24,66 @@ const projects = [
     title: 'Multi-Post Stories Gain+Glory',
     img: 'assets/images/placeholder-image.png',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    liveLink: 'https://aliatayee.github.io/Portfolio-mobile-version-skeleton-/',
-    github: 'https://github.com/aliatayee/Portfolio-mobile-version-skeleton-',
+    liveLink: 'https://aliatayee.github.io/portfolio-microverse/',
+    github: 'https://github.com/aliatayee/portfolio-microverse',
     techstack: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+    modal_techstack_desktop: ['Codekit','Github','Javascript','Bootstrap','Terminal','Codepen'],
+    modal_techstack_mobile: ['Codekit','Github','Javascript'],
   },
   {
     id: 2,
     title: 'Multi-Post Stories Gain+Glory',
     img: 'assets/images/placeholder-image.png',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    liveLink: 'https://aliatayee.github.io/Portfolio-mobile-version-skeleton-/',
-    github: 'https://github.com/aliatayee/Portfolio-mobile-version-skeleton-',
+    liveLink: 'https://aliatayee.github.io/portfolio-microverse/',
+    github: 'https://github.com/aliatayee/portfolio-microverse',
     techstack: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+    modal_techstack_desktop: ['Codekit','Github','Javascript','Bootstrap','Terminal','Codepen'],
+    modal_techstack_mobile: ['Codekit','Github','Javascript'],
   },
   {
     id: 3,
     title: 'Multi-Post Stories Gain+Glory',
     img: 'assets/images/placeholder-image.png',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    liveLink: 'https://aliatayee.github.io/Portfolio-mobile-version-skeleton-/',
-    github: 'https://github.com/aliatayee/Portfolio-mobile-version-skeleton-',
+    liveLink: 'https://aliatayee.github.io/portfolio-microverse/',
+    github: 'https://github.com/aliatayee/portfolio-microverse',
     techstack: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+    modal_techstack_desktop: ['Codekit','Github','Javascript','Bootstrap','Terminal','Codepen'],
+    modal_techstack_mobile: ['Codekit','Github','Javascript'],
   },
   {
     id: 4,
     title: 'Multi-Post Stories Gain+Glory',
     img: 'assets/images/placeholder-image.png',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    liveLink: 'https://aliatayee.github.io/Portfolio-mobile-version-skeleton-/',
-    github: 'https://github.com/aliatayee/Portfolio-mobile-version-skeleton-',
+    liveLink: 'https://aliatayee.github.io/portfolio-microverse/',
+    github: 'https://github.com/aliatayee/portfolio-microverse',
     techstack: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+    modal_techstack_desktop: ['Codekit','Github','Javascript','Bootstrap','Terminal','Codepen'],
+    modal_techstack_mobile: ['Codekit','Github','Javascript'],
   },
   {
     id: 4,
     title: 'Multi-Post Stories Gain+Glory',
     img: 'assets/images/placeholder-image.png',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    liveLink: 'https://aliatayee.github.io/Portfolio-mobile-version-skeleton-/',
-    github: 'https://github.com/aliatayee/Portfolio-mobile-version-skeleton-',
+    liveLink: 'https://aliatayee.github.io/portfolio-microverse/',
+    github: 'https://github.com/aliatayee/portfolio-microverse',
     techstack: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+    modal_techstack_desktop: ['Codekit','Github','Javascript','Bootstrap','Terminal','Codepen'],
+    modal_techstack_mobile: ['Codekit','Github','Javascript'],
   },
   {
     id: 4,
     title: 'Multi-Post Stories Gain+Glory',
     img: 'assets/images/placeholder-image.png',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-    liveLink: 'https://aliatayee.github.io/Portfolio-mobile-version-skeleton-/',
-    github: 'https://github.com/aliatayee/Portfolio-mobile-version-skeleton-',
+    liveLink: 'https://aliatayee.github.io/portfolio-microverse/',
+    github: 'https://github.com/aliatayee/portfolio-microverse',
     techstack: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+    modal_techstack_desktop: ['Codekit','Github','Javascript','Bootstrap','Terminal','Codepen'],
+    modal_techstack_mobile: ['Codekit','Github','Javascript'],
   },
 
 ];
@@ -88,7 +100,7 @@ window.onload = () => {
                         <li>${project.techstack[3]}</li>
                     </ul>
                     <div class="see-project">
-                        <button id="modal-btn" onclick="openModal(projects[${index}])">See project</button>
+                        <button id="modal-btn" onclick="openModal(${index})">See project</button>
                     </div>
                 </div>
       `;
@@ -96,47 +108,43 @@ window.onload = () => {
 };
 
 /* eslint-disable */
-function openModal() {
-  const gridContainer = document.getElementById('modal-container').innerHTML += `
+function openModal(i) {
+  const modal = document.getElementById('modal-container');
+  modal.innerHTML += `
   <div id="myModal" class="modal">
   <div class="modal-content">
-                    <span id="close" class="close" onclick="closeModal()">&times;</span>
+                    <span id="close" class="close" onclick="closeModal(${i})">&times;</span>
 
                     <div class="modal-img img-desktop"><img src="assets/images/modal-banner.svg" alt="project 1"></div>
                     <div class="modal-img img-mobile"><img src="assets/images/mobile-banner.svg" alt="project 1"></div>
                     <div class="row modal-header" id="modalHead">
-                        <div class="modal-title-container"><h3 class=" modal-title">Keeping track of hundreds of components</h3></div>
+                        <div class="modal-title-container"><h3 class=" modal-title">${projects[i].title}</h3></div>
                         <div class="desk-btns">
-                            <button class="modal-btn">See Live <i class="fa-solid fa-arrow-up-right-from-square"></i></button>
-                            <button class="modal-btn">See Source <i class="fa-brands fa-github"></i></button>
+                            <button class="modal-btn"><a href="${projects[i].liveLink}" class="button__link" target="_blank"> See Live <i class="fa-solid fa-arrow-up-right-from-square"></i></a></button>
+                            <button class="modal-btn"><a href="${projects[i].github}" class="button__link" target="_blank">See Source <i class="fa-brands fa-github"></i></a></button>
                         </div>
                     </div> 
                     <div class="stucks-modal">
                         <ul class=" modal-desktop-btn">
-                            <li>Codekit</li>
-                            <li>Github</li>
-                            <li>Javascript</li>
-                            <li>Bootstrap</li>
-                            <li>Terminal</li>
-                            <li>Codepen</li>
+                            <li>${projects[i].modal_techstack_desktop[0]}</li>
+                            <li>${projects[i].modal_techstack_desktop[1]}</li>
+                            <li>${projects[i].modal_techstack_desktop[2]}</li>
+                            <li>${projects[i].modal_techstack_desktop[3]}</li>
+                            <li>${projects[i].modal_techstack_desktop[4]}</li>
+                            <li>${projects[i].modal_techstack_desktop[5]}</li>
                         </ul>
                     </div>
                     <div class="stucks-modal-mobile">
                         <ul class=" modal-desktop-btn">
-                            <li>Codekit</li>
-                            <li>Github</li>
-                            <li>Javascript</li>
+                            <li>${projects[i].modal_techstack_desktop[0]}</li>
+                            <li>${projects[i].modal_techstack_desktop[1]}</li>
+                            <li>${projects[i].modal_techstack_desktop[2]}</li>
                         </ul>
                     </div>
-                    <p class="modal-p">Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
-                        1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem
-                        Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
-                        type and scrambled it 1960s with the relea</p>
+                    <p class="modal-p">${projects[i].description}</p>
                         <div class="mobile-btns">
-                          <button class="modal-btn">See Live <i class="fa-solid fa-arrow-up-right-from-square"></i></button>
-                          <button class="modal-btn">See Source <i class="fa-brands fa-github"></i></button>
+                          <button class="modal-btn"><a href="${projects[i].liveLink}" class="button__link" target="_blank">See Live <i class="fa-solid fa-arrow-up-right-from-square"></i></a></button>
+                          <button class="modal-btn"><a href="${projects[i].github}" class="button__link" target="_blank"> See Source <i class="fa-brands fa-github"></i></a></button>
                       </div>
                 </div>
                 </div>
@@ -145,14 +153,14 @@ function openModal() {
 // When the user clicks on <span> (x), close the modal
 
 function closeModal() {
-  const modal = document.getElementById('myModal');
-  modal.style.display = 'none';
+  const modal = document.getElementById('modal-container');
+  modal.innerHTML ='';
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  const modal = document.getElementById('myModal');
+  const modal = document.getElementById('modal-container');
   if (event.target === modal) {
-    modal.style.display = 'none';
+    document.getElementById('modal-container').innerHTML +='';
   }
 };
