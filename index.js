1c6582c5f61166156a3e51af887e6bdc6245465b
udeaghad <dozie.udeagha@gmail.com>
2022-05-12 09:49:47 +0100

    Add popup close modal button

--- index.js
+++ index.js
@@ -101,3 +101,16 @@ function openModal() {
   gridContainer.style.display = 'block';
 }
 // When the user clicks on <span> (x), close the modal
+
+function closeModal() {
+  const modal = document.getElementById('myModal');
+  modal.style.display = 'none';
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  const modal = document.getElementById('myModal');
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
