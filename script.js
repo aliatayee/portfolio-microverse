b5f95d5678af5fcf5260d8dc89f5caf7c9ea0a89
udeaghad <dozie.udeagha@gmail.com>
2022-05-14 09:22:26 +0100

    add local storage setItem

--- script.js
+++ script.js
@@ -195,3 +195,18 @@ if (localStorage.getItem('formData') !== null) {
 
 // saving data to local storage
 
+// getting form input fields and textarea
+const formElements = document.querySelectorAll('input, textarea');
+// looping through the elements
+formElements.forEach((element) => {
+  // displaying the formData values to input fields
+  element.value = formData[element.name];
+  // applying input event listener on elements
+  element.addEventListener('input', (e) => {
+    // setting the values of input fields to the respective keys in object
+    formData[e.target.name] = e.target.value;
+    // saving the data in local storage
+    localStorage.setItem('formData', JSON.stringify(formData));
+  });
+});
+
